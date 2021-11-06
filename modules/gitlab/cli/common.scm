96e6089906a26b77b703ba2deb2c8f5fdd3c5a01
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-11-06 21:02:41 +0300

    modules/gitlab/cli/common.scm: Add to the repository
    * modules/gitlab/cli/common.scm: Add to the repository.
    * modules/gitlab/cli/user.scm, utils/gitlab-cli.in: Use the new module.

---
+++ modules/gitlab/cli/common.scm
@@ -0,0 +1,45 @@
+(define-module (gitlab cli common)
+  #:export (print
+            print-many
+            string->boolean))
+
+
+(define (print result fields)
+  (let ((fields (and fields (string-split fields #\,))))
+    (for-each (lambda (rec)
+                (let ((key   (car rec))
+                      (value (cdr rec)))
+                  (if (or (not fields)
+                          (member key fields))
+                      (format #t "~20a: ~a~%"
+                              key
+                              (cond
+                               ((equal? value #f)
+                                "false")
+                               ((equal? value #t)
+                                "true")
+                               (else
+                                value))))))
+              result)))
+
+(define (print-many data fields)
+  (if fields
+      (let ((fields (string-split fields #\,)))
+        (pretty-print (map (lambda (user)
+                             (filter (lambda (rec) (member (car rec) fields))
+                                     user))
+                           data)))
+      (pretty-print data)))
+
+
+
+(define (string->boolean str)
+  (cond
+   ((string=? str "true")
+    #t)
+   ((string=? str "false")
+    #f)
+   (else
+    (error "Wrong boolean value (expecting 'true' or 'false')" str))))
+
+;;; common.scm ends here.
--- modules/gitlab/cli/user.scm
+++ modules/gitlab/cli/user.scm
@@ -6,49 +6,8 @@
   #:use-module (ice-9 regex)
   #:use-module (web uri)
   #:use-module (gitlab)
-  #:export (handle-user-command
-            print
-            print-many
-            string->boolean))
-
-
-;; Helper procedures.
-
-(define (print result fields)
-  (let ((fields (and fields (string-split fields #\,))))
-    (for-each (lambda (rec)
-                (let ((key   (car rec))
-                      (value (cdr rec)))
-                  (if (or (not fields)
-                          (member key fields))
-                      (format #t "~20a: ~a~%"
-                              key
-                              (cond
-                               ((equal? value #f)
-                                "false")
-                               ((equal? value #t)
-                                "true")
-                               (else
-                                value))))))
-              result)))
-
-(define (print-many data fields)
-  (if fields
-      (let ((fields (string-split fields #\,)))
-        (pretty-print (map (lambda (user)
-                             (filter (lambda (rec) (member (car rec) fields))
-                                     user))
-                           data)))
-      (pretty-print data)))
-
-(define (string->boolean str)
-  (cond
-   ((string=? str "true")
-    #t)
-   ((string=? str "false")
-    #f)
-   (else
-    (error "Wrong boolean value (expecting 'true' or 'false')" str))))
+  #:use-module (gitlab cli common)
+  #:export (handle-user-command))
 
 
 
